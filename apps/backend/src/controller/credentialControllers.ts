af5897db0144864d9b85890971284c89a3cb0af6
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-09-14 17:46:31 +0530

    added getCredentialById  controller

--- apps/backend/src/controller/credentialControllers.ts
+++ apps/backend/src/controller/credentialControllers.ts
@@ -41,3 +41,40 @@ export const createCredential = async (req: AuthRequest, res: Response) => {
     return res.status(500).json({ message: "Failed to create credential." });
   }
 };
+
+export const getCredentialById = async (req: AuthRequest, res: Response) => {
+  const ownerId = req.user?.id;
+  if (!ownerId) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({
+      message: "Credential id must be passed in the params",
+    });
+  }
+
+  try {
+    const dataSource = await getAppDataSource();
+    const credentialsRepository = dataSource.getRepository(Credential);
+    const credential = await credentialsRepository.findOneBy({
+      id,
+      user: { id: ownerId },
+    });
+    if (!credential)
+      return res
+        .status(500)
+        .json({ message: "Credential with specific ID not found" });
+    const { data, ...otherDetails } = credential;
+    return res.status(200).json({
+      otherDetails,
+    });
+  } catch (error) {
+    console.error("Error fetching credential by ID:", error);
+    return res
+      .status(404)
+      .json({
+        message: "Failed to find credential or you do not have access.",
+      });
+  }
+};
