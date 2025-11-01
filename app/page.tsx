3a9822a293dd49b547ec364c6d4335aace699605
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-11-01 22:55:11 +0530

    better auth and trpc protected routes integrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "./trpc/server";
 
 export default async function Home() {
-  const usr = await caller.hello()
+  await requireAuth()
+  const usr = await caller.getUsers()
+  console.log(usr)
+  // console.log(usr)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         {JSON.stringify(usr)}
