ec43780ab149c6610b84e06d45bc504b9fdfd124
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-11-05 13:29:35 +0530

    workflow routes added

---
+++ app/features/workflows/server/route.ts
@@ -0,0 +1,70 @@
+import { createTRPCRouter, protectedProcedure } from "@/app/trpc/init";
+import prisma from "@/lib/db";
+import z from "zod";
+
+export const workflowsRouter = createTRPCRouter({
+  create: protectedProcedure.mutation(({ ctx }) => {
+    return prisma.workflow.create({
+      data: {
+        name: "TODO",
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+
+  remove: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return prisma.workflow.delete({
+        where: {
+          id: input.id,
+          userId: ctx.auth.user.id,
+        },
+      });
+    }),
+
+  updateName: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return prisma.workflow.update({
+        where: {
+          id: input.id,
+          userId: ctx.auth.user.id,
+        },
+        data: {
+          name: input.name,
+        },
+      });
+    }),
+
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return prisma.workflow.findUnique({
+        where: {
+          id: input.id,
+          userId: ctx.auth.user.id,
+        },
+      });
+    }),
+  getMany: protectedProcedure.mutation(({ ctx }) => {
+    return prisma.workflow.findMany({
+      where: {
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
+});
