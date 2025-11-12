55552a58f93f9e7751697e26caa194b47bd9a911
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-11-12 11:05:07 +0530

    Add ky dependency and enhance HTTP request executor to handle response data

--- features/executions/components/http-request/executor.ts
+++ features/executions/components/http-request/executor.ts
@@ -1,15 +1,45 @@
 import { NodeExecutor } from "@/features/executions/type";
+import { NonRetriableError } from "inngest";
+import ky, {type Options as KyOptions} from 'ky'
 
-type HttpRequestData = Record<string, unknown>;
-
+type HttpRequestData = {
+  endpoint? : string ,
+  method? : "GET" | "POST" | "PUT" | "DELETE",
+  body?:string
+}
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
+  data,
   nodeId,
   context,
   step,
 }) => {
   // TODO: Publish "loading" state for HTTP request
+  if(!data.endpoint) {
+    throw new NonRetriableError("HTTP Request node not configured")
+  }
 
-  const result = await step.run("http-request", async () => context);
+  const result = await step.run("http-request", async () => {
+    const endpoint = data.endpoint!;
+    const method = data.method || 'GET'
+    const options : KyOptions = {method}
+
+    if(["POST" , "PUT" , "PATCH"] .includes((method))) {
+      options.body = data.body;
+    }
+
+    const response = await ky(endpoint, options);
+    const contentType = response.headers.get('content-type')
+    const responseData  = contentType?.includes('application/json')?await response.json():await response.text()
+
+    return {
+        ...context ,
+        httpResponse : {
+          status: response.status,
+          statusText : response.statusText,
+          data: responseData
+        }
+    }
+  });
 
   // TODO: Publish "success" state for HTTP request
 
