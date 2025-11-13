13c7cf488b3479acb20b377e9ba81e2a3d524222
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-11-13 22:36:50 +0530

    Add Google Form webhook handling and update executor registry

--- features/executions/lib/executor-registry.ts
+++ features/executions/lib/executor-registry.ts
@@ -1,12 +1,15 @@
 import { NodeType } from "@/lib/generated/prisma";
 import { NodeExecutor } from "../type";
-import { manualTriggerExecutor } from "@/features/triggers/manual-trigger/executor";
 import { httpRequestExecutor } from "../components/http-request/executor";
+import { manualTriggerExecutor } from "@/features/triggers/google-form-trigger/executor";
+import { googleFormTriggerExecutor } from "@/features/triggers/manual-trigger/executor";
 
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.INITIAL]: manualTriggerExecutor,
   [NodeType.HTTP_REQUEST]: httpRequestExecutor,
+  [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
+
 };
 
 export const getExecutor = (type: NodeType): NodeExecutor => {
