e4bcff850e5d73dc85e7cdb778dc83a1d8cd8475
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-11-22 02:22:36 +0530

    feat: implement executions feature with CRUD operations, add execution status handling, and integrate with TRPC

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { credentialsRouter } from "@/features/credentials/server/route";
 import { createTRPCRouter } from "../init";
 
 import { workflowsRouter } from "@/features/workflows/server/route";
+import { executionsRouter } from "@/features/executions/server/routes";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
   credential: credentialsRouter,
+  executions: executionsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
