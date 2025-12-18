46a9aecbe4e1664d427e060554b60b65c90ae99c
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-12-19 00:24:08 +0530

    add trsuted origin s

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
-// If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
+  trustedOrigins : [
+    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+  ],
   emailAndPassword: {
     enabled: true,
     autoSignIn:true
   },
-  // socialProviders: {
-  //   google: {
-  //     clientId: process.env.GOOGLE_CLIENT_ID as string,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  //   },
-  // },
+  
 });
